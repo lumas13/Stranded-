454bf1d3c719f157052603df767ce4b7b01c5b00
Eli <72849929+lumas13@users.noreply.github.com>
2021-02-03 16:39:35 +0800

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject Player;
+    public Vector3 posOff;
+    public Vector3 topView;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, Player.transform.position + posOff, 0.1f);
+
+        if (Input.GetKey(KeyCode.Tab)) //Changes view of camera to top down
+        {
+            transform.position = Vector3.Lerp(transform.position, Player.transform.position + topView, 1f);
+            transform.rotation = Quaternion.Euler(90, 0, 0); 
+        }
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            transform.position = Vector3.Lerp(transform.position, Player.transform.position + posOff, 0.1f);
+            transform.rotation = Quaternion.Euler(13, 0, 0);
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+    public GameObject BulleSpawn;
+
+    public float playerSpeed;
+    public float rotateSpeed;
+
+    Animator animator;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Shooting();
+    }
+
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            //Debug.Log("Test");
+            transform.Translate(Vector3.forward * playerSpeed * Time.deltaTime);
+            animator.SetBool("isRunning", true);
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            //Debug.Log("Test");
+            transform.Translate(Vector3.back * playerSpeed * Time.deltaTime);
+            animator.SetBool("isRunning", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
+        {
+            //Debug.Log("Test");
+            animator.SetBool("isRunning", false);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, -rotateSpeed * Time.deltaTime, 0));
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, rotateSpeed * Time.deltaTime, 0));
+        }
+    }
+
+    void Shooting()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(BulletPrefab, BulleSpawn.transform.position, transform.rotation);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Key"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
